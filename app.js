7f18f92af3c9206582e5bd2cb2c4361e1e7d2692
Pavel <pavel.brzezinski.1@ens.etsmtl.ca>
2018-03-24 17:46:03 -0400

    Changed path of html

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 app.use(function (req, res) {
-  fs.readFile('HTMLPage.html', function(err, data) {
+  fs.readFile('public/html/HTMLPage.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
