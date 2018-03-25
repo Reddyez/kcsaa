3c5dc45e7b948b5a204839c16f1547cefb584516
Pavel <pavel.brzezinski.1@ens.etsmtl.ca>
2018-03-25 00:56:46 -0400

    Added articles page

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quecéça' });
+  res.render('articles', { title: 'Article page' });
 });
 
 module.exports = router;
